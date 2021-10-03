50723da78a3b874d02ff30aeb91661cbe19c8018
Luke Butler <lukepbutler@gmail.com>
2021-10-03 19:54:35 -0400

    getError allocating only 1 byte of memory
    The getError function should have been allocating EN_MAXMSG (256) bytes of memory each time it tried to access an error code.
    Previously this was only 1 byte allocated so each time the function was run there was an overflow.
    This was particularly causing issues when a model had negative pressure and each time step was bringing up a warning

--- packages/epanet-js/test/Project/HydraulicAnalysisFunctions.test.ts
+++ packages/epanet-js/test/Project/HydraulicAnalysisFunctions.test.ts
@@ -4,11 +4,15 @@ import { InitHydOption } from '../../src/enum';
 import fs from 'fs';
 
 const net1 = fs.readFileSync(__dirname + '/../data/net1.inp', 'utf8');
+const net2 = fs.readFileSync(
+  __dirname + '/../data/AllElementsSmallNetwork.inp',
+  'utf8'
+);
 
 const ws = new Workspace();
 
-describe('Epanet Network Node Functions', () => {
-  describe('Impliment Methods', () => {
+describe('Epanet Hydraulic Analysis Functions', () => {
+  describe('Simple Hydraulic Methods', () => {
     test('solve hydraulic run', () => {
       ws.writeFile('net1.inp', net1);
       const model = new Project(ws);
@@ -44,6 +48,71 @@ describe('Epanet Network Node Functions', () => {
       const epanetMagicNumber = new DataView(bin.buffer).getInt32(0, true);
 
       expect(epanetMagicNumber).toEqual(516114521);
+      expect(tStep).toEqual(0);
+    });
+  });
+
+  describe('Previously failing models in epanet-js', () => {
+    test('solve a single hydraulic run', () => {
+      ws.writeFile('net1.inp', net2);
+      const model = new Project(ws);
+
+      model.open('net1.inp', 'net1.rpt', 'out.bin');
+      model.solveH();
+      model.saveH();
+
+      const bin = ws.readFile('out.bin', 'binary');
+      const epanetMagicNumber = new DataView(bin.buffer).getInt32(0, true);
+
+      expect(epanetMagicNumber).toEqual(516114521);
+      model.close();
+    });
+    test('solve two hydraulic runs in same workspace', () => {
+      ws.writeFile('net2.inp', net2);
+      const model = new Project(ws);
+      model.open('net2.inp', 'report2.rpt', 'out2.bin');
+
+      model.openH();
+      model.initH(InitHydOption.SaveAndInit);
+
+      let tStep = Infinity;
+      do {
+        model.runH();
+        tStep = model.nextH();
+      } while (tStep > 0);
+
+      model.saveH();
+      model.closeH();
+
+      const bin = ws.readFile('out2.bin', 'binary');
+      const epanetMagicNumber = new DataView(bin.buffer).getInt32(0, true);
+
+      expect(epanetMagicNumber).toEqual(516114521);
+      expect(tStep).toEqual(0);
+
+      const ws2 = new Workspace();
+      ws2.writeFile('net3.inp', net2);
+
+      const model2 = new Project(ws2);
+
+      model2.open('net3.inp', 'report3.rpt', 'out3.bin');
+
+      model2.openH();
+      model2.initH(InitHydOption.SaveAndInit);
+
+      tStep = Infinity;
+      do {
+        model2.runH();
+        tStep = model2.nextH();
+      } while (tStep > 0);
+
+      model2.saveH();
+      model2.closeH();
+
+      const bin2 = ws.readFile('out3.bin', 'binary');
+      const epanetMagicNumber2 = new DataView(bin2.buffer).getInt32(0, true);
+
+      expect(epanetMagicNumber2).toEqual(516114521);
       expect(tStep).toEqual(0);
     });
   });
