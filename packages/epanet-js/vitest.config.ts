65caeebfe094f5c2fb146f8b40094cf6266ef15e
lbutler <lukepbutler@gmail.com>
2025-05-05 22:08:44 -0400

    pnpm workspaces

---
+++ packages/epanet-js/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    coverage: {
+      provider: "v8",
+    },
+  },
+});
